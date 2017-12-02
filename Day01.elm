a51a008e30c412f3dde0fcafafaa8f651c4f4e52
ohanhi <ossi.hanhinen@gmail.com>
2017-12-02 22:07:47 +0200

    Add part two of day 1

--- Day01.elm
+++ Day01.elm
@@ -1,5 +1,6 @@
 module Main exposing (main)
 
+import Array
 import Html exposing (Html, text)
 
 
@@ -41,11 +42,37 @@ gather first acc remaining =
             gather first nextAcc (b :: rest)
 
 
+
+-- PART 2
+
+
+sum2 : List Int -> Int
+sum2 numbers =
+    let
+        array =
+            Array.fromList numbers
+
+        length =
+            Array.length array
+
+        halfwayAcross fromIdx =
+            Array.get ((fromIdx + length // 2) % length) array
+    in
+    array
+        |> Array.toIndexedList
+        |> List.filter (\( idx, num ) -> halfwayAcross idx == Just num)
+        |> List.map Tuple.second
+        |> List.sum
+
+
 main : Html msg
 main =
     { shouldBe3 = sum [ 1, 1, 2, 2 ]
     , shouldBe4 = sum [ 1, 1, 1, 1 ]
     , shouldBe9 = sum [ 9, 1, 2, 1, 2, 1, 2, 9 ]
+    , part2_shouldBe6 = sum2 [ 1, 2, 1, 2 ]
+    , part2_shouldBe0 = sum2 [ 1, 2, 2, 1 ]
+    , part2_shouldBe4 = sum2 [ 1, 2, 3, 4, 2, 5 ]
     }
         |> toString
         |> text
